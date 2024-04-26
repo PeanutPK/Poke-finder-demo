8ea52a21f457b691a56b925839c19a0d4315f0a5
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-04-26 11:10:17 +0700

    Fix: real shortest path

--- bfs.py
+++ bfs.py
@@ -14,12 +14,11 @@ adj = {}
 for key, value in vertex_data.items():
     adj[key] = Vertex(key, value)
 
-mark = {key: False for key in adj}
-parent = {key: -1 for key in adj}
-dist = {key: 9999 for key in adj}
-
 
 def bfs(start, end):
+    mark = {key: False for key in adj}
+    parent = {key: -1 for key in adj}
+    dist = {key: 9999 for key in adj}
     queue = deque()
     queue.append(start)
     dist[start] = 0
@@ -27,10 +26,11 @@ def bfs(start, end):
 
     while queue:
         current = queue.popleft()
-        print("Visiting:", current)
+        # uncomment all the print code below for debugging the paths
+        # print("Visiting:", current)
         if current == end:
             print("Reached end vertex:", end)
-            # Construct path from start to end
+            # Construct a path from start to end vertex
             path = []
             while current != -1:
                 path.append(current)
@@ -38,10 +38,11 @@ def bfs(start, end):
             path.reverse()
             print("Path from start to end:", path)
             return path
+
         for neighbor in adj[current].child:
-            print("Checking neighbor:", neighbor)
+            # print("Checking neighbor:", neighbor)
             if not mark[neighbor]:
-                print("Marking neighbor:", neighbor)
+                # print("Marking neighbor:", neighbor)
                 mark[neighbor] = True
                 parent[neighbor] = current
                 dist[neighbor] = dist[current] + 1
@@ -54,9 +55,24 @@ def bfs(start, end):
 if __name__ == '__main__':
     # Starting BFS from 'Viridian City' to all Geodude capture area by walking
     start_vertex = 'Viridian City'
-    capture_area = {"Mt. Moon": None, "Rock Tunnel": None, "Victory Road": None}
-    end_vertex = 'Mt. Moon'
+    capture_area = {"Mt. Moon": None, "Rock Tunnel": None,
+                    "Victory Road": None}
+    print("Paths are:\n")
     for area_name in capture_area.keys():
         end_vertex = area_name
         value = bfs(start_vertex, end_vertex)
+        print()
         capture_area[area_name] = value
+
+    shortest_area = []
+    shortest_path = []
+    for area_name, path in capture_area.items():
+        if not shortest_path:
+            shortest_area = area_name
+            shortest_path = path
+        elif len(shortest_path) > len(path):
+            shortest_area = area_name
+            shortest_path = path
+
+    print(f"Shortest path is from {shortest_path[0]} to {shortest_path[-1]}:\n"
+          + " -> ".join(shortest_path))
